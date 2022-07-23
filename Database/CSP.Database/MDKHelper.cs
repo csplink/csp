eed6656abc6437d9b4bfb762caa48498a560435f
小权一句两句 <xqyjlj@126.com>
2022-07-24 01:08:23 +0800

    添加Keil工程文件xml文件解析配置

---
+++ Database/CSP.Database/MDKHelper.cs
@@ -0,0 +1,9 @@
+﻿using CSP.Database.Models.MDK;
+
+namespace CSP.Database
+{
+    public class MDKHelper
+    {
+        public static ProjectModel Load(string path) => ProjectModel.Load(path);
+    }
+}
