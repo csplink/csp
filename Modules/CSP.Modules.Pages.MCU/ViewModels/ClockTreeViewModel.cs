396775ad923142bccdbbea13fc8670399e6372a9
小权一句两句 <xqyjlj@126.com>
2022-08-24 23:10:04 +0800

    添加时钟树

---
+++ Modules/CSP.Modules.Pages.MCU/ViewModels/ClockTreeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Prism.Events;
+using Prism.Mvvm;
+using Prism.Regions;
+
+namespace CSP.Modules.Pages.MCU.ViewModels
+{
+    public class ClockTreeViewModel : BindableBase
+    {
+        private readonly IEventAggregator _eventAggregator;
+        private readonly IRegionManager _regionManager;
+
+        public ClockTreeViewModel(IRegionManager regionManager, IEventAggregator eventAggregator) {
+            _regionManager = regionManager;
+            _eventAggregator = eventAggregator;
+        }
+    }
+}
