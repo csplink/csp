b53bf2ca6f25ab11009901df171feef33057e633
小权一句两句 <xqyjlj@126.com>
2022-09-08 23:12:25 +0800

    添加ClockView

---
+++ Modules/CSP.Modules.Pages.MCU/ViewModels/Components/Config/ClockViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Prism.Events;
+using Prism.Mvvm;
+using Prism.Regions;
+
+namespace CSP.Modules.Pages.MCU.ViewModels.Components.Config
+{
+    public class ClockViewModel : BindableBase, INavigationAware
+    {
+        #region INavigationAware
+
+        public bool IsNavigationTarget(NavigationContext navigationContext) {
+            return false;
+        }
+
+        public void OnNavigatedFrom(NavigationContext navigationContext) {
+        }
+
+        public void OnNavigatedTo(NavigationContext navigationContext) {
+        }
+
+        #endregion INavigationAware
+
+        private readonly IEventAggregator _eventAggregator;
+
+        public ClockViewModel(IEventAggregator eventAggregator) {
+            _eventAggregator = eventAggregator;
+        }
+    }
+}
