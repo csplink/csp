c2ceba3b83ce0a067de8d80d42b130cac75f26cf
小权一句两句 <xqyjlj@126.com>
2022-09-12 22:11:55 +0800

    添加属性Clock

--- Modules/CSP.Modules.Pages.MCU/ViewModels/Components/Config/ClockViewModel.cs
+++ Modules/CSP.Modules.Pages.MCU/ViewModels/Components/Config/ClockViewModel.cs
@@ -1,4 +1,8 @@
-﻿using Prism.Events;
+﻿using CSP.Components.ValuePropertyGrid;
+using CSP.Events;
+using CSP.Modules.Pages.MCU.Tools;
+using CSP.Utils;
+using Prism.Events;
 using Prism.Mvvm;
 using Prism.Regions;
 
@@ -16,6 +20,8 @@ namespace CSP.Modules.Pages.MCU.ViewModels.Components.Config
         }
 
         public void OnNavigatedTo(NavigationContext navigationContext) {
+            _eventAggregator.GetEvent<PropertyEvent>().Publish(null);
+            _eventAggregator.GetEvent<PropertyEvent>().Publish(Property);
         }
 
         #endregion INavigationAware
@@ -24,6 +30,39 @@ namespace CSP.Modules.Pages.MCU.ViewModels.Components.Config
 
         public ClockViewModel(IEventAggregator eventAggregator) {
             _eventAggregator = eventAggregator;
+
+            var clockMap = DescriptionHelper.GetMap("Clock");
+            var clockIP = DescriptionHelper.GetIP("Clock");
+            foreach (var mode in clockIP.ModeMap) {
+                foreach (var parameter in clockIP.ModeMap[mode.Key].Parameters) {
+                    var map = new ObservableDictionary<string, string>();
+
+                    foreach (var value in parameter.Value.Values) {
+                        if (clockMap.Total.ContainsKey(value)) {
+                            map.Add(value, clockMap.Total[value]);
+                        }
+                    }
+
+                    var model = new DictionaryEditorModel {
+                        Source = map
+                    };
+                    model.PropertyChanged += (sender, e) => {
+                        if (sender is not DictionaryEditorModel)
+                            return;
+
+                        switch (e.PropertyName) {
+                            case "Value": {
+                                }
+
+                                break;
+                        }
+                    };
+                    Property.Details.Add(parameter.Key, model);
+                    Property.Attributes.Add(parameter.Key, clockMap.Attributes[parameter.Key]);
+                }
+            }
         }
+
+        public PropertyDetails Property { get; } = new();
     }
 }
