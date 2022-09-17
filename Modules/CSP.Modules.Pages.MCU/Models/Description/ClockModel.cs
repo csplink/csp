e6ac3df785f2d04e3ca43b6a9af8151ec780fdcc
小权一句两句 <xqyjlj@126.com>
2022-09-18 00:01:12 +0800

    Create ClockModel.cs

---
+++ Modules/CSP.Modules.Pages.MCU/Models/Description/ClockModel.cs
@@ -0,0 +1,58 @@
+﻿using System.Xml.Serialization;
+
+namespace CSP.Modules.Pages.MCU.Models.Description
+{
+    [XmlRoot("Clock", IsNullable = false)]
+    public class ClockModel
+    {
+        [XmlIgnore]
+        public ControlModel Controls { get; set; }
+
+        [XmlArray("Controls")]
+        [XmlArrayItem("Control")]
+        public ControlModel ControlsTemp { get; set; }
+
+        [XmlAttribute]
+        public float Height { get; set; }
+
+        [XmlIgnore]
+        public RectModel Rects { get; set; }
+
+        [XmlArray("Rects")]
+        [XmlArrayItem("Rect")]
+        public RectModel RectsTemp { get; set; }
+
+        [XmlAttribute]
+        public float Width { get; set; }
+
+        public class ControlModel
+        {
+            [XmlAttribute]
+            public int ID { get; set; }
+
+            [XmlAttribute]
+            public string Name { get; set; }
+
+            [XmlAttribute]
+            public string Type { get; set; }
+        }
+
+        public class RectModel
+        {
+            [XmlAttribute]
+            public float Height { get; set; }
+
+            [XmlAttribute]
+            public int ID { get; set; }
+
+            [XmlAttribute]
+            public float Width { get; set; }
+
+            [XmlAttribute]
+            public float X { get; set; }
+
+            [XmlAttribute]
+            public float Y { get; set; }
+        }
+    }
+}
