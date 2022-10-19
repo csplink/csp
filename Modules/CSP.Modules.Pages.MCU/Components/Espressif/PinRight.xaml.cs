86e82c34f1708599ba5c8efbd74dc211dec9ae82
小权一句两句 <xqyjlj@126.com>
2022-10-19 23:39:53 +0800

    添加PinRight

---
+++ Modules/CSP.Modules.Pages.MCU/Components/Espressif/PinRight.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CSP.Modules.Pages.MCU.Components.Espressif
+{
+    public partial class PinRight
+    {
+        public PinRight() {
+            InitializeComponent();
+        }
+    }
+}
