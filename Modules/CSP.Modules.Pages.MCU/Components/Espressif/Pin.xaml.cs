f00328e665b05114c1c73c42e51dbfb443cbb611
小权一句两句 <xqyjlj@126.com>
2022-10-19 22:30:51 +0800

    添加Pin

--- Modules/CSP.Modules.Pages.MCU/Components/Espressif/Pin.xaml.cs
+++ Modules/CSP.Modules.Pages.MCU/Components/Espressif/Pin.xaml.cs
@@ -1,9 +1,31 @@
-﻿namespace CSP.Modules.Pages.MCU.Components.Espressif
+﻿using CSP.Modules.Pages.MCU.Enums;
+using System.Windows;
+
+namespace CSP.Modules.Pages.MCU.Components.Espressif
 {
     public partial class Pin
     {
+        public static readonly DependencyProperty DirectionProperty = DependencyProperty.Register("Direction",
+            typeof(DirectionEnum),
+            typeof(Pin),
+            new FrameworkPropertyMetadata(DirectionEnum.Top, OnDirectionChanged) { BindsTwoWayByDefault = true });
+
         public Pin() {
             InitializeComponent();
+        }
+
+        public DirectionEnum Direction {
+            get => (DirectionEnum)GetValue(DirectionProperty);
+            set => SetValue(DirectionProperty, value);
+        }
+
+        private static void OnDirectionChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+            if (d is not Pin pin)
+                return;
+            if (e.NewValue is not DirectionEnum direction)
+                return;
+
+            pin.ContentTemplate = pin.Resources[$"Pin{direction.ToString()}"] as DataTemplate;
         }
     }
 }
