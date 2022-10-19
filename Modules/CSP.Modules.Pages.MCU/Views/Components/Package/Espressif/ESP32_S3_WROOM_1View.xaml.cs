4bc88e52f1eee1cf19e82de5464d23c1e9026375
小权一句两句 <xqyjlj@126.com>
2022-10-19 23:41:41 +0800

    ESP32_S3_WROOM_1命名为ESP32_S3_WROOM_1View（符合命名规则）

--- Modules/CSP.Modules.Pages.MCU/Views/Components/Package/Espressif/ESP32_S3_WROOM_1View.xaml.cs
+++ Modules/CSP.Modules.Pages.MCU/Views/Components/Package/Espressif/ESP32_S3_WROOM_1View.xaml.cs
@@ -1,9 +1,9 @@
 ﻿namespace CSP.Modules.Pages.MCU.Views.Components.Package.Espressif
 {
     // ReSharper disable once InconsistentNaming
-    public partial class ESP32_S3_WROOM_1
+    public partial class ESP32_S3_WROOM_1View
     {
-        public ESP32_S3_WROOM_1() {
+        public ESP32_S3_WROOM_1View() {
             InitializeComponent();
         }
     }
