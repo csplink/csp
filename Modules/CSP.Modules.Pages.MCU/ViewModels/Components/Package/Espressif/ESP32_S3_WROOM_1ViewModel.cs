27ef771f6d2a73397c38c21ee780376c1e331510
小权一句两句 <xqyjlj@126.com>
2022-10-22 16:23:00 +0800

    Modules\CSP.Modules.Pages.MCU\ViewModels\Components\Package\Espressif\ESP32_S3_WROOM_1ViewModel.cs: ESP32_S3_WROOM_1的引脚列表长度为40.

--- Modules/CSP.Modules.Pages.MCU/ViewModels/Components/Package/Espressif/ESP32_S3_WROOM_1ViewModel.cs
+++ Modules/CSP.Modules.Pages.MCU/ViewModels/Components/Package/Espressif/ESP32_S3_WROOM_1ViewModel.cs
@@ -8,8 +8,8 @@ namespace CSP.Modules.Pages.MCU.ViewModels.Components.Package.Espressif
     public class ESP32_S3_WROOM_1ViewModel
     {
         public ESP32_S3_WROOM_1ViewModel() {
-            if (Pins.Length != 144) {
-                MessageBoxUtil.Error($"读取引脚数据失败，LQFP144的引脚列表长度不为144，读取结果为{Pins.Length}");
+            if (Pins.Length != 40) {
+                MessageBoxUtil.Error($"读取引脚数据失败，ESP32_S3_WROOM_1的引脚列表长度不为40，读取结果为{Pins.Length}");
             }
         }
 
