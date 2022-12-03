982132fbb39522b9f7d8f7334e9f74f31eba62a9
小权一句两句 <xqyjlj@126.com>
2022-12-03 15:41:12 +0800

    删除pin方向，因为bga引脚为圆形，无方向之分

--- Modules/CSP.Modules.Pages.MCU/Components/LFBGA/PinCommon.xaml.cs
+++ Modules/CSP.Modules.Pages.MCU/Components/LFBGA/PinCommon.xaml.cs
@@ -1,12 +1,13 @@
 ﻿namespace CSP.Modules.Pages.MCU.Components.LFBGA
 {
-    public partial class PinLeft
+    public partial class PinCommon
     {
-        public PinLeft() {
+        public PinCommon() {
             InitializeComponent();
             PinName = ButtonPin;
             PinNote = TextBlockNote;
             RightContextMenu = ButtonPinContextMenu;
+            IsDirection = false;
         }
     }
 }
