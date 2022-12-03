bb1437063dfea9f0203d140dd8def0823e0b9edd
小权一句两句 <xqyjlj@126.com>
2022-12-03 15:36:12 +0800

    添加LFBGA337View

---
+++ Modules/CSP.Modules.Pages.MCU/Views/Components/Package/LFBGA337View.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CSP.Modules.Pages.MCU.Views.Components.Package
+{
+    public partial class LFBGA337View
+    {
+        public LFBGA337View() {
+            InitializeComponent();
+        }
+    }
+}
