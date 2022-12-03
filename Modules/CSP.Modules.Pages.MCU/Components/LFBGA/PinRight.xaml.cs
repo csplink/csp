407130a12bdb9bf77a7e22525e7a4d00125232c8
小权一句两句 <xqyjlj@126.com>
2022-12-03 15:41:11 +0800

    添加LFBGA Pin

---
+++ Modules/CSP.Modules.Pages.MCU/Components/LFBGA/PinRight.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CSP.Modules.Pages.MCU.Components.LFBGA
+{
+    public partial class PinRight
+    {
+        public PinRight() {
+            InitializeComponent();
+            PinName = ButtonPin;
+            PinNote = TextBlockNote;
+            RightContextMenu = ButtonPinContextMenu;
+        }
+    }
+}
