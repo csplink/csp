d2fdad7531d8edac497cc027a7448c787770226c
小权一句两句 <xqyjlj@126.com>
2022-12-03 15:36:12 +0800

    当传入null时，也应该返回null

--- Utils/CSP.Utils/Converter/IsStringNullOrEmptyConverter.cs
+++ Utils/CSP.Utils/Converter/IsStringNullOrEmptyConverter.cs
@@ -8,10 +8,11 @@ namespace CSP.Utils.Converter
     public class IsStringNullOrEmptyConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
-            if (value is string str)
-                return str.IsNullOrEmpty();
-
-            throw new ArgumentException("IsStringNullOrEmptyConverter can only be used String");
+            return value switch {
+                null => true,
+                string str => str.IsNullOrEmpty(),
+                _ => throw new ArgumentException("IsStringNullOrEmptyConverter can only be used String")
+            };
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
