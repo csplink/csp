f4a978294be01fa51f0c1049f775aeb24b7d9815
小权一句两句 <xqyjlj@126.com>
2022-12-03 15:36:11 +0800

    添加Converter

---
+++ Utils/CSP.Utils/Converter/IsNullConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace CSP.Utils.Converter
+{
+    public class IsNullConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            return (value == null);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            DebugUtil.Assert(false, new InvalidOperationException("IsNullConverter can only be used OneWay."));
+            return null;
+        }
+    }
+}
---
+++ Utils/CSP.Utils/Converter/IsStringNullOrEmptyConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using CSP.Utils.Extensions;
+
+namespace CSP.Utils.Converter
+{
+    public class IsStringNullOrEmptyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            if (value is string str)
+                return str.IsNullOrEmpty();
+
+            throw new ArgumentException("IsStringNullOrEmptyConverter can only be used String");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new InvalidOperationException("IsStringNullOrEmptyConverter can only be used OneWay.");
+        }
+    }
+}
