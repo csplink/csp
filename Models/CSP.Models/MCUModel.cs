b0552dd67c2d5c197190972519b5f3b4829586ab
xqyjlj <xqyjlj@126.com>
2022-12-04 22:59:25 +0800

    🦄 refactor(Models/CSP.Models/MCUModel.cs): change xml to yaml

--- Models/CSP.Models/MCUModel.cs
+++ Models/CSP.Models/MCUModel.cs
@@ -1,132 +1,62 @@
 ﻿using CSP.Utils;
-using CSP.Utils.Extensions;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Windows;
-using System.Xml.Serialization;
 
 namespace CSP.Models
 {
-    [XmlRoot("MCU", IsNullable = false)]
     public class MCUModel
     {
-        [XmlArray("Applications")]
-        [XmlArrayItem("Document")]
-        public DocumentModel[] Applications { get; set; }
-
-        [XmlAttribute]
         public string ClockTree { get; set; }
-
-        [XmlAttribute]
         public string Company { get; set; }
-
-        public string CompanyUrl { get; set; }
-
-        [XmlArray("DataSheets")]
-        [XmlArrayItem("Document")]
-        public DocumentModel[] DataSheets { get; set; }
-
-        [XmlArray("Errata")]
-        [XmlArrayItem("Document")]
-        public DocumentModel[] Errata { get; set; }
-
-        public HALModel HAL { get; set; }
-
-        [XmlAttribute]
+        public Dictionary<string, string> CompanyUrl { get; set; }
+        public Dictionary<string, DocumentModel[]> Documents { get; set; }
+        public string HAL { get; set; }
         public bool HasPowerPad { get; set; }
-
-        public string Illustrate { get; set; }
-
-        public string Introduction { get; set; }
-
-        [XmlAttribute]
-        public string IoType { get; set; }
-
-        [XmlAttribute]
+        public Dictionary<string, string> Illustrate { get; set; }
+        public Dictionary<string, string> Introduction { get; set; }
         public string Line { get; set; }
-
-        [XmlArray("Modules")]
-        [XmlArrayItem("Module")]
-        public ModuleModel[] Modules { get; set; }
-
-        [XmlAttribute]
+        public Dictionary<string, ModuleModel[]> Modules { get; set; }
         public string Name { get; set; }
-
-        [XmlAttribute]
         public string Package { get; set; }
-
-        [XmlAttribute]
         public float Price { get; set; }
-
-        [XmlArray("Programs")]
-        [XmlArrayItem("Document")]
-        public DocumentModel[] Programs { get; set; }
-
-        [XmlArray("References")]
-        [XmlArrayItem("Document")]
-        public DocumentModel[] References { get; set; }
-
-        public string RepositoryUrl { get; set; }
-
-        [XmlAttribute]
         public string Series { get; set; }
-
-        public string Url { get; set; }
+        public Dictionary<string, string> Url { get; set; }
 
         public static MCUModel Load(string path) {
-            DebugUtil.Assert(!path.IsNullOrEmpty(), new ArgumentNullException(nameof(path)), "path不能为空");
-            DebugUtil.Assert(File.Exists(path), new FileNotFoundException(nameof(path)), $"{path}: 不存在");
+            DebugUtil.Assert(!string.IsNullOrEmpty(path), new ArgumentNullException(nameof(path)));
 
-            if (path == null) return null;
-            if (!File.Exists(path)) return null;
+            if (!File.Exists(path))
+                return null;
 
-            var deserializer = new XmlSerializer(typeof(MCUModel));
-            var reader = new StreamReader(path);
+            var deserializer = new YamlDotNet.Serialization.Deserializer();
 
             MCUModel rtn;
             try {
-                rtn = (MCUModel)deserializer.Deserialize(reader);
+                using (var reader = new StreamReader(path)) {
+                    rtn = deserializer.Deserialize<MCUModel>(reader);
+                }
             }
             catch (InvalidOperationException e) {
-                MessageBox.Show(e.Message, "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show(e.Message, "error", MessageBoxButton.OK, MessageBoxImage.Error);
                 return null;
             }
 
-            DebugUtil.Assert(rtn != null, new ArgumentNullException(nameof(rtn)), "XML反序列化失败");
+            DebugUtil.Assert(rtn != null, new ArgumentNullException("MCU.YAML"), "YAML deserialization failed");
             return rtn;
         }
 
         public class DocumentModel
         {
-            [XmlAttribute]
             public string Name { get; set; }
-
-            public string Url { get; set; }
-        }
-
-        public class HALModel
-        {
-            [XmlAttribute]
-            public string Name { get; set; }
-
-            [XmlAttribute]
-            public string Version { get; set; }
+            public Dictionary<string, string> Url { get; set; }
         }
 
         public class ModuleModel
         {
-            [XmlArray("Categories")]
-            [XmlArrayItem("Category")]
-            public CategoryModel[] Categories { get; set; }
-
-            [XmlAttribute]
+            public Dictionary<string, string> Description { get; set; }
             public string Name { get; set; }
-
-            public class CategoryModel
-            {
-                [XmlAttribute]
-                public string Name { get; set; }
-            }
         }
     }
 }
