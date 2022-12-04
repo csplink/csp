a4bff690222bd36a0c41c34decad0addb10b2554
xqyjlj <xqyjlj@126.com>
2022-12-04 19:57:54 +0800

    🦄 refactor(Database/CSP.Database/Models/MCU/RepositoryModel.cs): change xml to yaml

--- Database/CSP.Database/Models/MCU/RepositoryModel.cs
+++ Database/CSP.Database/Models/MCU/RepositoryModel.cs
@@ -1,170 +1,96 @@
 ﻿using CSP.Utils;
-using CSP.Utils.Extensions;
 using Prism.Mvvm;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Windows;
-using System.Xml.Serialization;
-
-// ReSharper disable UnusedAutoPropertyAccessor.Global
 
 namespace CSP.Database.Models.MCU
 {
-    [XmlRoot("Repository", IsNullable = false)]
+    using repository_t = Dictionary<string, Dictionary<string, Dictionary<string, Dictionary<string, RepositoryModel.MCUModel>>>>;
+
     public class RepositoryModel
     {
-        [XmlArray("Companies")]
-        [XmlArrayItem("Company")]
-        public CompanyModel[] Companies { get; set; }
-
-        internal static RepositoryModel Load(string path) {
-            DebugUtil.Assert(!path.IsNullOrEmpty(), new ArgumentNullException(nameof(path)));
+        public static repository_t Load(string path) {
+            DebugUtil.Assert(!string.IsNullOrEmpty(path), new ArgumentNullException(nameof(path)));
 
             if (!File.Exists(path))
                 return null;
 
-            var deserializer = new XmlSerializer(typeof(RepositoryModel));
-            var reader = new StreamReader(path);
-            RepositoryModel rtn;
+            var deserializer = new YamlDotNet.Serialization.Deserializer();
+
+            repository_t rtn;
             try {
-                rtn = (RepositoryModel)deserializer.Deserialize(reader);
+                using (var reader = new StreamReader(path)) {
+                    rtn = deserializer.Deserialize<repository_t>(reader);
+                }
             }
             catch (InvalidOperationException e) {
-                MessageBox.Show(e.Message, "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show(e.Message, "error", MessageBoxButton.OK, MessageBoxImage.Error);
                 return null;
             }
 
-            DebugUtil.Assert(rtn != null, new ArgumentNullException(nameof(rtn)), "XML反序列化失败");
+            DebugUtil.Assert(rtn != null, new ArgumentNullException("Repository.YAML"), "YAML deserialization failed");
+
             if (rtn == null)
                 return null;
 
-            // 补充辅助值
-            foreach (var company in rtn.Companies) {
-                foreach (var series in company.Series) {
-                    foreach (var line in series.Lines) {
-                        foreach (var mcu in line.MCU) {
-                            mcu.Company = company.Name;
-                            mcu.Series = series.Name;
-                            mcu.Line = line.Name;
-
-                            //TODO 需要在此处加入价格表数据更新
+            foreach (var (companyName, companies) in rtn) {
+                foreach (var (seriesName, series) in companies) {
+                    foreach (var (lineName, lines) in series) {
+                        foreach (var (mcuName, mcu) in lines) {
+                            mcu.Company = companyName;
+                            mcu.Series = seriesName;
+                            mcu.Line = lineName;
+                            mcu.Name = mcuName;
+                            //TODO Price list data updater needs to be added here
                         }
                     }
                 }
             }
-
             return rtn;
         }
 
-        public class CompanyModel
+        public class MCUModel
         {
-            [XmlAttribute]
+            public string Company { get; set; }
+            public string Core { get; set; }
+            public CurrentModel Current { get; set; }
+            public float Flash { get; set; }
+            public float Frequency { get; set; }
+            public int IO { get; set; }
+            public string Line { get; set; }
             public string Name { get; set; }
+            public string Package { get; set; }
+            public PeripheralModel[] Peripherals { get; set; }
+            public float Price { get; set; }
+            public float Ram { get; set; }
+            public string Series { get; set; }
+            public TemperatureModel Temperature { get; set; }
+            public VoltageModel Voltage { get; set; }
 
-            [XmlArray("Series")]
-            [XmlArrayItem("Series")]
-            public SeriesModel[] Series { get; set; }
-
-            public class SeriesModel
+            public class CurrentModel
             {
-                [XmlArray("Lines")]
-                [XmlArrayItem("Line")]
-                public LineModel[] Lines { get; set; }
+                public float Lowest { get; set; }
+                public float Run { get; set; }
+            }
 
-                [XmlAttribute]
-                public string Name { get; set; }
+            public class PeripheralModel
+            {
+                public int Count { get; set; }
+                public string Type { get; set; }
+            }
 
-                public class LineModel
-                {
-                    [XmlArray("MCU")]
-                    [XmlArrayItem("MCU")]
-                    public MCUModel[] MCU { get; set; }
+            public class TemperatureModel : BindableBase
+            {
+                public float Max { get; set; }
+                public float Min { get; set; }
+            }
 
-                    [XmlAttribute]
-                    public string Name { get; set; }
-
-                    public class MCUModel
-                    {
-                        [XmlIgnore]
-                        public string Company { get; set; }
-
-                        [XmlAttribute]
-                        public string Core { get; set; }
-
-                        public CurrentModel Current { get; set; }
-
-                        [XmlAttribute]
-                        public float Flash { get; set; }
-
-                        [XmlAttribute]
-                        public float Frequency { get; set; }
-
-                        [XmlAttribute]
-                        public int IO { get; set; }
-
-                        [XmlIgnore]
-                        public string Line { get; set; }
-
-                        [XmlAttribute]
-                        public string Name { get; set; }
-
-                        [XmlAttribute]
-                        public string Package { get; set; }
-
-                        [XmlArray("Peripherals")]
-                        [XmlArrayItem("Peripheral")]
-                        public PeripheralModel[] Peripherals { get; set; }
-
-                        [XmlIgnore]
-                        public float Price { get; set; }
-
-                        [XmlAttribute]
-                        public float Ram { get; set; }
-
-                        [XmlIgnore]
-                        public string Series { get; set; }
-
-                        public TemperatureModel Temperature { get; set; }
-
-                        public VoltageModel Voltage { get; set; }
-
-                        public class CurrentModel
-                        {
-                            [XmlAttribute]
-                            public float Lowest { get; set; }
-
-                            [XmlAttribute]
-                            public float Run { get; set; }
-                        }
-
-                        public class PeripheralModel
-                        {
-                            [XmlAttribute]
-                            public int Number { get; set; }
-
-                            [XmlAttribute]
-                            public string Type { get; set; }
-                        }
-
-                        public class TemperatureModel : BindableBase
-                        {
-                            [XmlAttribute]
-                            public float Max { get; set; }
-
-                            [XmlAttribute]
-                            public float Min { get; set; }
-                        }
-
-                        public class VoltageModel : BindableBase
-                        {
-                            [XmlAttribute]
-                            public float Max { get; set; }
-
-                            [XmlAttribute]
-                            public float Min { get; set; }
-                        }
-                    }
-                }
+            public class VoltageModel : BindableBase
+            {
+                public float Max { get; set; }
+                public float Min { get; set; }
             }
         }
     }
