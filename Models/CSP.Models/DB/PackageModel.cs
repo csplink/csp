3714ef89b01b4652932f685e783a55d0e235dbbb
xqyjlj <xqyjlj@126.com>
2022-12-07 22:17:47 +0800

    ✨ feat(Models\CSP.Models\DB\PackageModel.cs): must have latest

--- Models/CSP.Models/DB/PackageModel.cs
+++ Models/CSP.Models/DB/PackageModel.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Windows;
 
 namespace CSP.Models.DB
@@ -46,6 +47,11 @@ namespace CSP.Models.DB
             foreach (var descriptionName in descriptionNames) {
                 rtn.Description.Add(CultureInfo.CurrentCulture.TextInfo.ToTitleCase(descriptionName), rtn!.Description[descriptionName]);
                 rtn.Description.Remove(descriptionName);
+            }
+            if (!rtn.Versions.Contains("latest")) {
+                var list = rtn!.Versions.ToList();
+                list.Add("latest");
+                rtn.Versions = list.ToArray();
             }
             return rtn;
         }
