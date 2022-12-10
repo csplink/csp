81af63344efabe26795d4010985e6bcb541e86a0
xqyjlj <xqyjlj@126.com>
2022-12-10 21:16:20 +0800

    ✨ feat(Models\CSP.Models\HAL\Config\ClockModel.cs): add ClockModel.cs

---
+++ Models/CSP.Models.Tests/HAL/Config/ClockModelTests.cs
@@ -0,0 +1,96 @@
+using System.IO;
+using CSP.Models.DB;
+using CSP.Models.HAL.Config;
+using CSP.Resources;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CSP.Models.Tests.HAL.Config;
+
+public class ClockModelTests
+{
+#if DEBUG
+    private readonly string _mode = "Debug";
+#else
+    private readonly string _mode = "Release";
+#endif
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public ClockModelTests(ITestOutputHelper testOutputHelper) {
+        _testOutputHelper = testOutputHelper;
+    }
+
+    public void Dispose() {
+        IniFile.Save();
+    }
+
+    [Fact]
+    public void Load() {
+        string   solutionDir = File.ReadAllLines("./SolutionDir.txt")[0];
+        string   dir         = $"{solutionDir}/Apps/CSP.Apps.Dev/bin/{_mode}/net6.0-windows/csp_repo/packages";
+        string[] dirs        = Directory.GetDirectories(dir);
+        foreach (string d in dirs) {
+            string[] files = Directory.GetFiles(d, "*.json");
+            foreach (string file in files) {
+                _testOutputHelper.WriteLine($"load file: {file}");
+                PackageModel package = PackageModel.Load(file);
+                foreach (string version in package.Versions) {
+                    dir =
+                        $"{solutionDir}/Apps/CSP.Apps.Dev/bin/{_mode}/net6.0-windows/csp_repo/repositories/{package.Category}/{package.Name}/{version}/config";
+                    if (!Directory.Exists(dir)) {
+                        continue;
+                    }
+
+                    string[] configDirs = Directory.GetDirectories(dir);
+                    foreach (string configDir in configDirs) {
+                        string pinoutPath = $"{configDir}/pinout.yml";
+                        if (!File.Exists(pinoutPath)) {
+                            continue;
+                        }
+
+                        string clockName = new DirectoryInfo(configDir).Name;
+                        string clockPath = configDir + $"/clock/{clockName}.yml";
+
+                        _testOutputHelper.WriteLine($"load file: {clockPath}");
+                        ClockModel clock = ClockModel.Load(clockPath);
+                        Assert.False(clock == null);
+                        Assert.False(clock.Width <= 0);
+                        Assert.False(clock.Height <= 0);
+                        Assert.False(clock.Controls == null);
+                        Assert.False(clock.Rects == null);
+                        Assert.False(clock.Ellipses == null);
+
+                        foreach (var (id, control) in clock.Controls) {
+                            Assert.False(id <= 0);
+                            Assert.False(control == null);
+                            Assert.False(control.Base == null);
+                            Assert.False(control.Base.Multiple < 0);
+                            Assert.False(control.Base.DefaultValue < 0);
+                            Assert.False(control.Base.DefaultIndex < 0);
+                            Assert.False(string.IsNullOrWhiteSpace(control.Base.Name));
+                            Assert.False(string.IsNullOrWhiteSpace(control.Base.Type));
+                        }
+
+                        foreach (var (id, rect) in clock.Rects) {
+                            Assert.False(id <= 0);
+                            Assert.False(rect == null);
+                            Assert.False(rect.X <= 0);
+                            Assert.False(rect.Y <= 0);
+                            Assert.False(rect.Width <= 0);
+                            Assert.False(rect.Height <= 0);
+                        }
+
+                        foreach (var (id, ellipse) in clock.Ellipses) {
+                            Assert.False(id <= 0);
+                            Assert.False(ellipse == null);
+                            Assert.False(ellipse.X <= 0);
+                            Assert.False(ellipse.Y <= 0);
+                            Assert.False(ellipse.Width <= 0);
+                            Assert.False(ellipse.Height <= 0);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Models/CSP.Models/HAL/Config/ClockModel.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using CSP.Utils;
+using YamlDotNet.Serialization;
+
+namespace CSP.Models.HAL.Config;
+
+public class ClockModel
+{
+    public float Width { get; set; }
+
+    public float Height { get; set; }
+
+    public Dictionary<int, ControlModel> Controls { get; set; }
+
+    public Dictionary<int, RectModel> Rects { get; set; }
+
+    public Dictionary<int, EllipseModel> Ellipses { get; set; }
+
+    public static ClockModel Load(string path) {
+        DebugUtil.Assert(!string.IsNullOrWhiteSpace(path), new ArgumentNullException(nameof(path)));
+
+        if (!File.Exists(path)) {
+            return null;
+        }
+
+        Deserializer deserializer = new();
+
+        ClockModel rtn;
+        try {
+            using (StreamReader reader = new(path)) {
+                rtn = deserializer.Deserialize<ClockModel>(reader);
+            }
+        }
+        catch (InvalidOperationException e) {
+            MessageBox.Show(e.Message, "error", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            return null;
+        }
+
+        DebugUtil.Assert(rtn != null, new ArgumentNullException("Clock.YAML"), "YAML deserialization failed");
+
+        return rtn;
+    }
+
+    public class ControlModel
+    {
+        public BaseModel Base { get; set; }
+
+        public class BaseModel
+        {
+            public string Name { get; set; }
+
+            public string Type { get; set; }
+
+            public int Multiple { get; set; }
+
+            public float DefaultValue { get; set; }
+
+            public int DefaultIndex { get; set; }
+
+            public string GroupName { get; set; }
+
+            public string Macro { get; set; }
+
+            public bool IsChecked { get; set; }
+        }
+    }
+
+    public class RectModel
+    {
+        public float X { get; set; }
+
+        public float Y { get; set; }
+
+        public float Width { get; set; }
+
+        public float Height { get; set; }
+    }
+
+    public class EllipseModel
+    {
+        public float X { get; set; }
+
+        public float Y { get; set; }
+
+        public float Width { get; set; }
+
+        public float Height { get; set; }
+    }
+}
