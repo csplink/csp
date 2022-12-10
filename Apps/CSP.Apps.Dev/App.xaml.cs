2a4299e7559696769a009fffad4f3c0a17338195
xqyjlj <xqyjlj@126.com>
2022-12-10 22:18:11 +0800

    ↩ revert(Apps\CSP.Apps.Dev): Temporarily block CSP.Modules.Pages.MCU.csproj

--- Apps/CSP.Apps.Dev/App.xaml.cs
+++ Apps/CSP.Apps.Dev/App.xaml.cs
@@ -1,56 +1,58 @@
-﻿using CSP.Apps.Dev.Views;
+﻿using System.Text;
+using System.Windows;
+using CSP.Apps.Dev.Views;
+using CSP.Modules.Dialogs.NewMCU;
 using CSP.Resources;
 using Prism.Ioc;
 using Prism.Modularity;
 using Prism.Regions;
 using Serilog;
 using Syncfusion.Windows.Tools.Controls;
-using System.Text;
-using System.Windows;
 
-namespace CSP.Apps.Dev
+namespace CSP.Apps.Dev;
+
+public partial class App
 {
-    public partial class App
-    {
-        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog) {
-            base.ConfigureModuleCatalog(moduleCatalog);
+    protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog) {
+        base.ConfigureModuleCatalog(moduleCatalog);
 
-            moduleCatalog.AddModule<CSPModule>();
+        moduleCatalog.AddModule<CSPModule>();
 
-            moduleCatalog.AddModule<Modules.Dialogs.NewMCU.NewMCUModule>();
+        moduleCatalog.AddModule<NewMCUModule>();
 
-            moduleCatalog.AddModule<Modules.Pages.MCU.MCUModule>();
-        }
+        // moduleCatalog.AddModule<Modules.Pages.MCU.MCUModule>();
+    }
 
-        protected override void ConfigureRegionAdapterMappings(RegionAdapterMappings regionAdapterMappings) {
-            base.ConfigureRegionAdapterMappings(regionAdapterMappings);
-            regionAdapterMappings.RegisterMapping(typeof(DockingManager), Container.Resolve<RegionAdapter.DockingManager.RegionAdapter>());
-        }
+    protected override void ConfigureRegionAdapterMappings(RegionAdapterMappings regionAdapterMappings) {
+        base.ConfigureRegionAdapterMappings(regionAdapterMappings);
+        regionAdapterMappings.RegisterMapping(typeof(DockingManager),
+            Container.Resolve<RegionAdapter.DockingManager.RegionAdapter>());
+    }
 
-        protected override Window CreateShell() {
-            return Container.Resolve<MainView>();
-        }
+    protected override Window CreateShell() {
+        return Container.Resolve<MainView>();
+    }
 
-        protected override void RegisterTypes(IContainerRegistry containerRegistry) {
-        }
+    protected override void RegisterTypes(IContainerRegistry containerRegistry) {
+    }
 
-        private void OnAppExit(object sender, ExitEventArgs e) {
-            IniFile.Save();
-            Log.CloseAndFlush();
-        }
+    private void OnAppExit(object sender, ExitEventArgs e) {
+        IniFile.Save();
+        Log.CloseAndFlush();
+    }
 
-        private void OnAppStartup(object sender, StartupEventArgs e) {
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Verbose()
-                .WriteTo.Debug()
-                .WriteTo.File(IniFile.PathLogFile,//文件保存路径
-                    outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss}] [{Level:u3}] {Message:lj}{NewLine}{Exception}",//输出日期格式
-                    rollingInterval: RollingInterval.Day,//日志按日保存
-                    rollOnFileSizeLimit: true, // 限制单个文件的最大长度
-                    encoding: Encoding.UTF8, // 文件字符编码
-                    retainedFileCountLimit: 10, // 最大保存文件数
-                    fileSizeLimitBytes: 10 * 1024) // 最大单个文件长度
-                .CreateLogger();
-        }
+    private void OnAppStartup(object sender, StartupEventArgs e) {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Verbose()
+            .WriteTo.Debug()
+            .WriteTo.File(IniFile.PathLogFile, //文件保存路径
+                outputTemplate:
+                "[{Timestamp:yyyy-MM-dd HH:mm:ss}] [{Level:u3}] {Message:lj}{NewLine}{Exception}", //输出日期格式
+                rollingInterval: RollingInterval.Day,                                              //日志按日保存
+                rollOnFileSizeLimit: true,                                                         // 限制单个文件的最大长度
+                encoding: Encoding.UTF8,                                                           // 文件字符编码
+                retainedFileCountLimit: 10,                                                        // 最大保存文件数
+                fileSizeLimitBytes: 10 * 1024)                                                     // 最大单个文件长度
+            .CreateLogger();
     }
 }
