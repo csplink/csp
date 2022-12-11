4b7619d2610917e93e49dda711b4a793f6995c4f
xqyjlj <xqyjlj@126.com>
2022-12-11 12:26:58 +0800

    ✨ feat(Utils\CSP.Utils\Util.cs): format and check url IsNullOrWhiteSpace

--- Utils/CSP.Utils/Util.cs
+++ Utils/CSP.Utils/Util.cs
@@ -1,23 +1,26 @@
 ﻿using System.Diagnostics;
 
-namespace CSP.Utils
-{
-    public class Util
-    {
-        public static void OpenUrl(string url) {
-            Process p = new();
-            p.StartInfo.FileName = "cmd.exe";
-            p.StartInfo.UseShellExecute = false;
-            p.StartInfo.RedirectStandardInput = true;
-            p.StartInfo.RedirectStandardOutput = false;
-            p.StartInfo.RedirectStandardError = true;
-            p.StartInfo.CreateNoWindow = true;
-            p.Start();
+namespace CSP.Utils;
 
-            p.StandardInput.WriteLine("start " + url + "&exit");
-            p.StandardInput.AutoFlush = true;
-            p.WaitForExit();
-            p.Close();
+public class Util
+{
+    public static void OpenUrl(string url) {
+        if (string.IsNullOrWhiteSpace(url)) {
+            return;
         }
+
+        Process p = new();
+        p.StartInfo.FileName               = "cmd.exe";
+        p.StartInfo.UseShellExecute        = false;
+        p.StartInfo.RedirectStandardInput  = true;
+        p.StartInfo.RedirectStandardOutput = false;
+        p.StartInfo.RedirectStandardError  = true;
+        p.StartInfo.CreateNoWindow         = true;
+        p.Start();
+
+        p.StandardInput.WriteLine("start " + url + "&exit");
+        p.StandardInput.AutoFlush = true;
+        p.WaitForExit();
+        p.Close();
     }
 }
