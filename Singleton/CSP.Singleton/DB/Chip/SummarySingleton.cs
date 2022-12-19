16e702753533216b4eea339e90ad5169315c451c
xqyjlj <xqyjlj@126.com>
2022-12-19 22:50:47 +0800

    ✨ feat(Singleton\CSP.Singleton\DB\Chip): add SummarySingleton, SummarySingletonInstance
    add Singleton\CSP.Singleton\DB\Chip\SummarySingleton.cs, Singleton\CSP.Singleton\DB\Chip\SummarySingletonInstance.cs

---
+++ Singleton/CSP.Singleton/DB/Chip/SummarySingleton.cs
@@ -0,0 +1,17 @@
+﻿using CSP.Models.DB.Chip;
+
+namespace CSP.Singleton.DB.Chip;
+
+public static class SummarySingleton
+{
+    private static readonly SummarySingletonInstance Instance = SummarySingletonInstance.Instance;
+
+    public static SummaryModel Summary => Instance.Summary;
+
+
+    public static bool Add(string path) {
+        Instance.Summary = SummaryModel.Load(path);
+
+        return Instance.Summary != null;
+    }
+}
---
+++ Singleton/CSP.Singleton/DB/Chip/SummarySingletonInstance.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CSP.Models.DB.Chip;
+
+namespace CSP.Singleton.DB.Chip;
+
+internal class SummarySingletonInstance
+{
+    private static readonly Lazy<SummarySingletonInstance> Lazy = new(static () => new SummarySingletonInstance());
+
+    private SummarySingletonInstance() {
+    }
+
+    public static SummarySingletonInstance Instance => Lazy.Value;
+
+    public SummaryModel Summary { get; set; }
+}
