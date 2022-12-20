2f440de6b5a51c7b00a451ceb2d6b049424f07df
xqyjlj <xqyjlj@126.com>
2022-12-20 21:27:15 +0800

    🎈 perf(Singleton\CSP.Singleton\Internal\PinConfigSingleton.cs): add Assert

--- Singleton/CSP.Singleton/Internal/PinConfigSingleton.cs
+++ Singleton/CSP.Singleton/Internal/PinConfigSingleton.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using CSP.Models.HAL.Config;
 using CSP.Models.Internal;
+using CSP.Utils;
 
 namespace CSP.Singleton.Internal;
 
@@ -10,15 +12,17 @@ public static class PinConfigSingleton
 
     public static Dictionary<string, PinConfigModel> PinConfigs => Instance.PinConfigs;
 
-    public static void Add(string name) {
+    private static void Add(string name) {
         if (!PinConfigs.ContainsKey(name)) {
             PinConfigs[name] = new PinConfigModel();
         }
     }
 
     public static void Add(string name, PinoutModel pinOut) {
+        DebugUtil.Assert(!string.IsNullOrWhiteSpace(name), new ArgumentNullException(nameof(name)), "name is null");
+        DebugUtil.Assert(pinOut == null, new ArgumentNullException(nameof(pinOut)), "pinOut is null");
         Add(name);
-        PinConfigs[name].Name.String     = name;
-        PinConfigs[name].Position.String = pinOut.Position.ToString();
+        PinConfigs[name!].Name.String    = name;
+        PinConfigs[name].Position.String = pinOut!.Position.ToString();
     }
 }
