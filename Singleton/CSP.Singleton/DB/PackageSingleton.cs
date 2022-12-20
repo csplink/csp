2c16c352701210362d2c0363f08b942688048118
xqyjlj <xqyjlj@126.com>
2022-12-20 22:44:20 +0800

    ✨ feat(Singleton\CSP.Singleton\DB\PackageSingleton.cs): add PackageSingleton

---
+++ Singleton/CSP.Singleton/DB/PackageSingleton.cs
@@ -0,0 +1,17 @@
+﻿using CSP.Models.DB;
+
+namespace CSP.Singleton.DB;
+
+public static class PackageSingleton
+{
+    private static readonly PackageSingletonInstance Instance = PackageSingletonInstance.Instance;
+
+    public static PackageModel Summary => Instance.Package;
+
+
+    public static bool Add(string path) {
+        Instance.Package = PackageModel.Load(path);
+
+        return Instance.Package != null;
+    }
+}
---
+++ Singleton/CSP.Singleton/DB/PackageSingletonInstance.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CSP.Models.DB;
+
+namespace CSP.Singleton.DB;
+
+internal class PackageSingletonInstance
+{
+    private static readonly Lazy<PackageSingletonInstance> Lazy = new(static () => new PackageSingletonInstance());
+
+    private PackageSingletonInstance() {
+    }
+
+    public static PackageSingletonInstance Instance => Lazy.Value;
+
+    public PackageModel Package { get; set; }
+}
