674cfb4c5350888415a561f861002f5e38b23423
xqyjlj <xqyjlj@126.com>
2022-12-20 21:03:49 +0800

    ✨ feat(Singleton\CSP.Singleton\Internal\ProjectSingleton.cs): add ProjectSingleton

---
+++ Singleton/CSP.Singleton/Internal/ProjectSingleton.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using CSP.Models.Internal;
+using CSP.Utils;
+
+namespace CSP.Singleton.Internal;
+
+public static class ProjectSingleton
+{
+    private static readonly ProjectSingletonInstance Instance = ProjectSingletonInstance.Instance;
+
+    public static ProjectModel Project => Instance.Project;
+
+    public static void ChangeDefine(string oldKey, string newKey, string newValue) {
+        DebugUtil.Assert(!string.IsNullOrWhiteSpace(oldKey) || !string.IsNullOrWhiteSpace(newKey),
+            new ArgumentNullException(nameof(oldKey) + " or " + nameof(newKey)), "oldKey 或者 newKey 不能均为空");
+        if (Project.DefinesI.ContainsKey(oldKey!)) {
+            Project.DefinesI.Remove(oldKey);
+        }
+
+        if (Project.DefinesI.ContainsKey(newKey!)) {
+            Project.DefinesI.Remove(newKey);
+        }
+
+        Project.DefinesI.Add(newKey, newValue);
+    }
+
+    public static bool IsDependence(IEnumerable<string> dependencies) {
+        if (dependencies == null) {
+            return true;
+        }
+
+        bool isDependence = true;
+        foreach (string dependence in dependencies) {
+            if (!Project.DefinesI.ContainsKey(dependence)) {
+                isDependence = false;
+            }
+        }
+
+        return isDependence;
+    }
+}
---
+++ Singleton/CSP.Singleton/Internal/ProjectSingletonInstance.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CSP.Models.Internal;
+
+namespace CSP.Singleton.Internal;
+
+internal class ProjectSingletonInstance
+{
+    private static readonly Lazy<ProjectSingletonInstance> Lazy = new(static () => new ProjectSingletonInstance());
+
+    private ProjectSingletonInstance() {
+    }
+
+    public static ProjectSingletonInstance Instance => Lazy.Value;
+
+    public ProjectModel Project { get; } = new();
+}
