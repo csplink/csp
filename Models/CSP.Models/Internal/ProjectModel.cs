d9d418731dfef43243c24bbfecbdb252821f4cb8
xqyjlj <xqyjlj@126.com>
2022-12-20 21:03:21 +0800

    ✨ feat(Models\CSP.Models\Internal\ProjectModel.cs): add ProjectModel

---
+++ Models/CSP.Models/Internal/ProjectModel.cs
@@ -0,0 +1,14 @@
+﻿using CSP.Events;
+using CSP.Utils;
+using YamlDotNet.Serialization;
+
+namespace CSP.Models.Internal;
+
+public class ProjectModel
+{
+    [YamlIgnore]
+    public ObservableDictionary<string, string> Defines { get; } = new();
+
+    [YamlIgnore]
+    public ObservableDictionary<string, PropertyDetails> Properties { get; } = new();
+}
