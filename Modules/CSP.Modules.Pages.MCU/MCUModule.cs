c322e045ff1525e4d080a197d1114660666f5159
xqyjlj <xqyjlj@126.com>
2023-01-08 17:40:32 +0800

    🌈 style: format header
    by psioniq.psi-header
    https://marketplace.visualstudio.com/items?itemName=psioniq.psi-header

--- Modules/CSP.Modules.Pages.MCU/MCUModule.cs
+++ Modules/CSP.Modules.Pages.MCU/MCUModule.cs
@@ -1,4 +1,27 @@
-﻿global using ip_t =
+﻿// Licensed under the Apache License, Version 2.0 (the "License");
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Copyright (C) 2022-2023 xqyjlj<xqyjlj@126.com>
+//
+// @author      xqyjlj
+// @file        MCUModule.cs
+//
+// Change Logs:
+// Date           Author       Notes
+// ------------   ----------   -----------------------------------------------
+// 2023-01-08     xqyjlj       initial version
+//
+
+global using ip_t =
     System.Collections.Generic.Dictionary<string, System.Collections.Generic.Dictionary<string, string[]>>;
 using CSP.Modules.Pages.MCU.Views;
 using CSP.Modules.Pages.MCU.Views.Components.Config;
