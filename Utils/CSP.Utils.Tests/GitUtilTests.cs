1371f2e133820037921aefb92607714b83e0427e
xqyjlj <xqyjlj@126.com>
2023-01-11 20:25:26 +0800

    ✨ feat(Utils/CSP.Utils/GitUtil.cs): add GitUtil

---
+++ Utils/CSP.Utils.Tests/GitUtilTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+
+namespace CSP.Utils.Tests;
+
+public class GitUtilTests
+{
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public GitUtilTests(ITestOutputHelper testOutputHelper) {
+        _testOutputHelper = testOutputHelper;
+    }
+
+    public void Dispose() {
+    }
+
+    [Fact]
+    public void GetGitVersionTest() {
+        string version = GitUtil.GetGitVersion();
+        _testOutputHelper.WriteLine(version);
+        Assert.False(string.IsNullOrWhiteSpace(version));
+    }
+}
