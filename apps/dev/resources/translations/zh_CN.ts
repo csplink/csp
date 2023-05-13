f53a1e081d835ae31cd76ca5dac171af8e484155
xqyjlj <xqyjlj@126.com>
2023-05-13 21:41:31 +0800

    ✨ feat(apps\dev\resources\translations\zh_CN.ts): update translations

--- apps/dev/resources/translations/zh_CN.ts
+++ apps/dev/resources/translations/zh_CN.ts
@@ -2,6 +2,144 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="zh_CN" sourcelanguage="en">
 <context>
+    <name>choose_mcu_dialog</name>
+    <message>
+        <location filename="../../dialogs/choose_mcu_dialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/choose_mcu_dialog.ui" line="37"/>
+        <source>芯片</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/choose_mcu_dialog.ui" line="60"/>
+        <source>特性</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/choose_mcu_dialog.ui" line="90"/>
+        <source>版本：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/choose_mcu_dialog.ui" line="103"/>
+        <source>0.0.1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/choose_mcu_dialog.ui" line="128"/>
+        <source>封装：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/choose_mcu_dialog.ui" line="135"/>
+        <source>xxxxx</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/choose_mcu_dialog.ui" line="160"/>
+        <source>价格（元/10K）：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/choose_mcu_dialog.ui" line="173"/>
+        <source>9999</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/choose_mcu_dialog.ui" line="189"/>
+        <source>停售</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/choose_mcu_dialog.ui" line="202"/>
+        <source>市场：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/choose_mcu_dialog.ui" line="227"/>
+        <source>此栏放芯片图</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/choose_mcu_dialog.ui" line="256"/>
+        <source>资源图</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/choose_mcu_dialog.ui" line="261"/>
+        <source>文档</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/choose_mcu_dialog.ui" line="266"/>
+        <source>购买</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/choose_mcu_dialog.ui" line="285"/>
+        <source>芯片名</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/choose_mcu_dialog.ui" line="290"/>
+        <source>市场动态</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/choose_mcu_dialog.ui" line="295"/>
+        <source>价格（元/10K）</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/choose_mcu_dialog.ui" line="300"/>
+        <source>封装</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/choose_mcu_dialog.ui" line="305"/>
+        <source>Flash</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/choose_mcu_dialog.ui" line="310"/>
+        <source>RAM</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/choose_mcu_dialog.ui" line="315"/>
+        <source>IO</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/choose_mcu_dialog.ui" line="320"/>
+        <source>频率</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/choose_mcu_dialog.ui" line="325"/>
+        <source>公司</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/choose_mcu_dialog.ui" line="330"/>
+        <source>内核</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/choose_mcu_dialog.ui" line="335"/>
+        <source>主科</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/choose_mcu_dialog.ui" line="340"/>
+        <source>亚科</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>home_view</name>
     <message>
         <location filename="../../views/home_view.ui" line="50"/>
