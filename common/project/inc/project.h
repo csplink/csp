c2b3e88484ad05cf165792b775c98a2e4129ea8d
xqyjlj <xqyjlj@126.com>
2023-05-26 00:11:56 +0800

    ✨ feat: add project

---
+++ common/project/inc/project.h
@@ -0,0 +1,53 @@
+/*
+ * ****************************************************************************
+ *  @author      xqyjlj
+ *  @file        project.h
+ *  @brief
+ *
+ * ****************************************************************************
+ *  @attention
+ *  Licensed under the GNU General Public License v. 3 (the "License");
+ *  You may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  Copyright (C) 2023-2023 xqyjlj<xqyjlj@126.com>
+ *
+ * ****************************************************************************
+ *  Change Logs:
+ *  Date           Author       Notes
+ *  ------------   ----------   -----------------------------------------------
+ *  2023-05-26     xqyjlj       initial version
+ */
+
+#ifndef COMMON_PROJECT_CSP_PROJECT_H
+#define COMMON_PROJECT_CSP_PROJECT_H
+
+#include <QObject>
+
+namespace csp {
+class project : public QObject {
+    Q_OBJECT
+
+public:
+    static project *get_instance();
+
+private:
+    project();
+    ~project() override;
+
+    project(const project &signal);
+    const project &operator=(const project &signal);
+
+private:
+    static project *_instance;
+};
+}  // namespace csp
+#endif  // COMMON_PROJECT_CSP_PROJECT_H
