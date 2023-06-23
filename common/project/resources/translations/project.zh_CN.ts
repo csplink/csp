06b6727513f6cdc9877f945baa704f5c5478bd04
xqyjlj <xqyjlj@126.com>
2023-06-23 18:03:39 +0800

    ✨ feat(common\project): add translations

---
+++ common/project/resources/translations/project.zh_CN.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>project</name>
+    <message>
+        <location filename="../../src/project.cpp" line="176"/>
+        <source>The project &lt;%1&gt; path is not a directory!</source>
+        <translation>工程 &lt;%1&gt; 路径不是目录！</translation>
+    </message>
+</context>
+</TS>
