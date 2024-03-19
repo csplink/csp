77972a4b0462cfac28c239faf48347e6e025c2e1
xqyjlj <xqyjlj@126.com>
2024-03-19 14:29:26 +0800

    ✨ feat(common): 添加多语言
    Signed-off-by: xqyjlj <xqyjlj@126.com>

---
+++ common/config/resources/translations/CspConfig.zh_TW.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_TW">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../src/Config.cpp" line="99"/>
+        <source>Critical</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/Config.cpp" line="99"/>
+        <source>The workspace &lt;%1&gt; path is not a directory!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
