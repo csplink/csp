88f5e1b87a72aec66d258c71784b1a12d32a3027
xqyjlj <xqyjlj@126.com>
2024-06-04 00:17:54 +0800

    暂存
    Signed-off-by: xqyjlj <xqyjlj@126.com>

--- tools/coder/csp-coder.py
+++ tools/coder/csp-coder.py
@@ -31,6 +31,8 @@ import time
 import os, sys, getopt
 import json
 import jinja2
+import importlib.util
+import glob
 
 script_dir = os.path.dirname(__file__)
 version = "v0.0.0.2"
@@ -47,11 +49,13 @@ def match_xmake_user(file_path: str) -> dict:
     if os.path.isfile(file_path):
         with open(file_path, "r", encoding='utf-8') as file:
             data = file.read()
-            for s in re.findall("----< add user code begin (.*)\n", data):
-                matcher = f"----< add user code begin {s}\n(.*)----> add user code end {s}\n"
+            for s in re.findall("----< add user code begin (.*), do not change this comment!\n", data):
+                matcher = f"----< add user code begin {s}, do not change this comment!\n(.*)----> add user code end {s}, do not change this comment!\n"
                 result = str.rstrip(re.findall(matcher, data, re.S)[0])
                 if result:
                     user_code[s] = str.rstrip(result)
+                    if user_code[s] != "":
+                        user_code[s] = user_code[s] + "\n"
                 else:
                     user_code[s] = ""
     return user_code
@@ -65,11 +69,13 @@ def match_cmake_user(file_path: str) -> dict:
     if os.path.isfile(file_path):
         with open(file_path, "r", encoding='utf-8') as file:
             data = file.read()
-            for s in re.findall("##==< add user code begin (.*)\n", data):
-                matcher = f"##==< add user code begin {s}\n(.*)##==> add user code end {s}\n"
+            for s in re.findall("##==< add user code begin (.*), do not change this comment!\n", data):
+                matcher = f"##==< add user code begin {s}, do not change this comment!\n(.*)##==> add user code end {s}, do not change this comment!\n"
                 result = str.rstrip(re.findall(matcher, data, re.S)[0])
                 if result:
                     user_code[s] = str.rstrip(result)
+                    if user_code[s] != "":
+                        user_code[s] = user_code[s] + "\n"
                 else:
                     user_code[s] = ""
     return user_code
@@ -83,11 +89,13 @@ def match_cfile_user(file_path: str) -> dict:
     if os.path.isfile(file_path):
         with open(file_path, "r", encoding='utf-8') as file:
             data = file.read()
-            for s in re.findall(r"/\*\*< add user code begin (.*) \*/", data):
-                matcher = rf"/\*\*< add user code begin {s} \*/\n(.*)/\*\*> add user code end {s} \*/"
+            for s in re.findall("/\*\*< add user code begin (.*), do not change this comment! \*/", data):
+                matcher = f"/\*\*< add user code begin {s}, do not change this comment! \*/\n(.*)/\*\*> add user code end {s}, do not change this comment! \*/"
                 result = str.rstrip(re.findall(matcher, data, re.S)[0])
                 if result:
                     user_code[s] = str.rstrip(result)
+                    if user_code[s] != "":
+                        user_code[s] = user_code[s] + "\n"
                 else:
                     user_code[s] = ""
     return user_code
@@ -145,14 +153,16 @@ def generate_mdk_arm_project(args: dict, target_file: str, min_version: str) -> 
     """
     project = args["Project"]
 
-    import coder
+    spec = importlib.util.spec_from_file_location("coder", f'{package_dir}/tools/coder/gen_mdk_arm.py')
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
 
     if os.path.isfile(target_file):
         tree = etree.parse(target_file)
     else:
         tree = None
 
-    return coder.generate_mdk_arm_project(copy.deepcopy(project), copy.deepcopy(min_version), copy.deepcopy(tree), keil)
+    return module.main(copy.deepcopy(project), copy.deepcopy(min_version), copy.deepcopy(tree), keil)
 
 
 def generate_project(env: jinja2.Environment, output_dir: str, args: dict):
@@ -190,32 +200,19 @@ def generate_project(env: jinja2.Environment, output_dir: str, args: dict):
         pass
 
 
-def param_check():
-    """
-    param check
-    """
-    pass
-
-
-def parse(project: dict) -> dict:
-    """
-    parse
-    """
-    import coder
-    data = coder.parse(copy.deepcopy(project))
-    return data
-
-
-def deploy(project: dict, output_dir: str) -> dict:
+def deploy(package_dir: str, project: dict, output_dir: str) -> dict:
     """
     deploy
     """
-    import coder
-    data = coder.deploy(copy.deepcopy(project), copy.deepcopy(output_dir))
+    spec = importlib.util.spec_from_file_location("coder", f'{package_dir}/tools/coder/deploy.py')
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+
+    data = module.main(copy.deepcopy(project), copy.deepcopy(output_dir))
     return data
 
 
-def main(project_file: str, output_dir: str, repository_dir: str):
+def main(project_file: str, output_dir: str, repository_dir: str, package_dir, toolchains_dir: str):
     """
     main
     """
@@ -225,30 +222,21 @@ def main(project_file: str, output_dir: str, repository_dir: str):
 
     hal = project_json["Hal"]
     modules = project_json["Modules"]
-    package_dir = f'{repository_dir}/library/{hal}/{project_json["HalVersion"]}'
+
+    if package_dir == "":
+        package_dir = f'{repository_dir}/library/{hal}/{project_json["HalVersion"]}'
 
     if not os.path.isdir(package_dir):
         print(f"error: {package_dir} is not directory! maybe package({hal}) not yet installed.")
         sys.exit(2)
 
-    sys.path.append(f'{package_dir}/tools/coder')
-
-    coder_data = parse(project_json)  # get coder data from hal coder.py
-    if project_json["ToolchainsVersion"] != "":
-        toolchains_path = f"{repository_dir}/toolchains/{project_json['Toolchains']}/{project_json['ToolchainsVersion']}"
-    else:
-        toolchains_path = ""
-
     data = {
-        "Base": {
-            "Author": "csplink coder",
-            "Version": version
-        },
+        "Author": "csplink coder",
+        "Version": version,
         "Project": project_json,
         "Time": time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()),
         "Year": time.strftime('%Y', time.localtime()),
-        "CoderData": coder_data,
-        "ToolchainsPath": toolchains_path,
+        "ToolchainsPath": toolchains_dir if toolchains_dir != "" else None,
     }
 
     if not os.path.isdir(f"{output_dir}/core/src/"):
@@ -256,8 +244,20 @@ def main(project_file: str, output_dir: str, repository_dir: str):
     if not os.path.isdir(f"{output_dir}/core/inc/csplink"):
         os.makedirs(f"{output_dir}/core/inc/csplink")
 
-    env = jinja2.Environment(
-        loader=jinja2.FileSystemLoader([f'{script_dir}/templates', f'{package_dir}/tools/coder/templates']))
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader(
+        [f'{script_dir}/templates', f'{package_dir}/tools/coder/templates']),
+                             line_comment_prefix="//")
+
+    filter_files = glob.glob(f"{package_dir}/tools/coder/filters/*.py")
+    for file in filter_files:
+        spec = importlib.util.spec_from_file_location(os.path.basename(file).split(".")[0], file)
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+        functions = [name for name in dir(module) if callable(getattr(module, name))]
+        for fun in functions:
+            if not fun.startswith("_"):
+                function = getattr(module, fun)
+                env.filters[fun] = function
 
     generate_cfile(env, "main", output_dir, data)
 
@@ -267,7 +267,7 @@ def main(project_file: str, output_dir: str, repository_dir: str):
 
     generate_project(env, output_dir, data)
 
-    deploy(project_json, output_dir)
+    deploy(package_dir, project_json, output_dir)
 
 
 def help():
@@ -280,11 +280,14 @@ def help():
     print("    -f, --file               csp project file path.")
     print("    -o, --output             set the output directory.")
     print("    -r, --repository         repository dir.")
+    print("    -d, --package_dir        package dir.")
+    print("    -t, --toolchains_dir     toolchains dir.")
 
 
 if __name__ == '__main__':
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hf:o:r:", ["help", "file=", "output=", "repository="])
+        opts, args = getopt.getopt(sys.argv[1:], "hf:o:r:d:t:",
+                                   ["help", "file=", "output=", "repository=", "package_dir=", "toolchains_dir="])
     except getopt.GetoptError:
         help()
         sys.exit(2)
@@ -292,6 +295,8 @@ if __name__ == '__main__':
     file = ""
     output = ""
     repository = ""
+    package_dir = ""
+    toolchains_dir = ""
     for opt, arg in opts:
         if opt in ("-h", "--help"):
             help()
@@ -302,8 +307,12 @@ if __name__ == '__main__':
             output = arg
         elif opt in ("-r", "--repository"):
             repository = arg
+        elif opt in ("-d", "--package_dir"):
+            package_dir = arg
+        elif opt in ("-t", "--toolchains_dir"):
+            toolchains_dir = arg
 
-    if file == "" or repository == "":
+    if file == "" or (repository == "" and package_dir == ""):
         help()
         sys.exit(2)
 
@@ -311,11 +320,19 @@ if __name__ == '__main__':
         print(f"error: {file} is not file!")
         sys.exit(2)
 
-    if not os.path.isdir(repository):
+    if repository != "" and (not os.path.isdir(repository)):
         print(f"error: {repository} is not directory!")
+        sys.exit(2)
+
+    if package_dir != "" and (not os.path.isdir(package_dir)):
+        print(f"error: {package_dir} is not directory!")
+        sys.exit(2)
+
+    if toolchains_dir != "" and (not os.path.isdir(toolchains_dir)):
+        print(f"error: {toolchains_dir} is not directory!")
         sys.exit(2)
 
     if output == "":
         output = os.path.dirname(file)
 
-    main(file, output, repository)
+    main(file, output, repository, package_dir, toolchains_dir)
